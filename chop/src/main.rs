d1308839c54023d4f51ea10f2f57e545fa2d423e
Brad Sz <BradSz@outlook.com>
2023-04-21 10:24:47 -0400

    Added unit test for default case

--- chop/src/main.rs
+++ chop/src/main.rs
@@ -6,7 +6,7 @@ use clap::Parser;
 struct Config {
     #[arg(short, long)]
     /// Wrap lines at boundary instead of truncating
-    wrap: bool,
+    wrap: Option<bool>,
 
     #[arg(short, long)]
     /// Chop after given number of characters instead of screen width
@@ -27,6 +27,19 @@ struct Config {
     #[arg(short, long, default_value = "2.0")]
     /// Minimum interval to requery if terminal size has been adjusted; ignored when `--characters` is specified
     update: Option<f32>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            wrap: Default::default(),
+            characters: Default::default(),
+            delimiter: Default::default(),
+            multiple: Default::default(),
+            offset: Default::default(),
+            update: Default::default(),
+        }
+    }
 }
 
 struct Limiter {
@@ -65,11 +78,11 @@ impl Limiter {
 
 fn run(
     config: &Config,
+    limiter: &mut Limiter,
     input: &mut impl std::io::BufRead,
     output: &mut impl std::io::Write,
 ) -> std::io::Result<()> {
     let mut buffer = String::new();
-    let mut limiter = Limiter::new(config);
     loop {
         buffer.clear();
         let nread = input.read_line(&mut buffer)?;
@@ -90,7 +103,7 @@ fn run(
             let subs = &s[..end].trim_end();
             writeln!(output, "{}", subs)?;
 
-            if config.wrap {
+            if config.wrap.unwrap_or(false) {
                 s = &s[end..];
             } else {
                 break;
@@ -99,11 +112,63 @@ fn run(
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use super::*;
+
+    fn c10(c: char) -> String {
+        String::from_str("[10char-x]")
+            .unwrap()
+            .replace("x", c.to_string().as_str())
+    }
+
+    fn get_termsize_10() -> Option<termsize::Size> {
+        Some(termsize::Size { rows: 0, cols: 10 })
+    }
+
+    #[test]
+    fn test_c10() {
+        assert_eq!("[10char-A]", c10('A').as_str());
+    }
+
+    #[test]
+    fn test_truncate_default() {
+        let config = Config::default();
+        let mut limiter = Limiter {
+            config: config,
+            get_termsize: get_termsize_10,
+        };
+
+        let input = format!(
+            "{}{}{}{}\n{}{}\n",
+            c10('A'),
+            c10('B'),
+            c10('C'),
+            c10('D'), // newline
+            c10('E'),
+            c10('F'), // newline
+        );
+        let exp: String = format!(
+            "{}\n{}\n",
+            c10('A'), // newline
+            c10('E')  // newline
+        );
+
+        let mut output: Vec<u8> = Vec::new();
+        run(&config, &mut limiter, &mut input.as_bytes(), &mut output).unwrap();
+
+        assert_eq!(exp.as_bytes(), output);
+    }
+}
+
 fn main() {
     let config = Config::parse();
 
     match run(
         &config,
+        &mut Limiter::new(&config),
         &mut std::io::stdin().lock(),
         &mut std::io::stdout().lock(),
     ) {
